d9f20a6797c47afccdd1b7ab2c7e5e2038567756
John Colagioia <john@colagioia.net>
2015-04-29 06:38:01 -0400

    Add table relationships

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,2 +1,11 @@
 class Document < ActiveRecord::Base
+  validates :type, presence: true, inclusion: { in: %w(zip rar tar 7z jpeg gif tiff png archive file), message: "%{value} is not a usable file." }
+  validates :digest, presence: true, length: { is: 64 }, format: { with: /^[0-9A-Fa-F]+$/, message: "Not a SHA-256 digest."}
+  validates :created, presence: true
+  validates :size, presence: true, numericality: { only_integer: true }
+  validates :folder, presence: true
+
+  has_many :inclusions
+  has_many :aliases
+  has_one :track
 end
