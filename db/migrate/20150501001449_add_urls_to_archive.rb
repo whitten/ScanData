70876baa0caf001e6b8869a01c885fd77546c932
John Colagioia <john@colagioia.net>
2015-05-01 07:53:27 -0400

    Add URLs to archive model

---
+++ db/migrate/20150501001449_add_urls_to_archive.rb
@@ -0,0 +1,8 @@
+class AddUrlsToArchive < ActiveRecord::Migration
+  def change
+    add_column :archives, :dcmUrl, :string
+    add_column :archives, :cbpUrl, :string
+    add_column :archives, :gcdUrl, :string
+    add_column :archives, :cbdbUrl, :string
+  end
+end
