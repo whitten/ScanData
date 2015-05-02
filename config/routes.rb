6028b31434d41ea14cfaa180fa15e6863b861e5d
John Colagioia <john@colagioia.net>
2015-05-02 07:29:17 -0400

    Add submission scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :submissions
   resources :documents
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
