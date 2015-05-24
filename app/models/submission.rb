fcfeb777a17483583706f077d5e90610fde7447f
John Colagioia <john@colagioia.net>
2015-05-24 07:34:45 -0400

    Add validation to submission model

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,4 +1,9 @@
 class Submission < ActiveRecord::Base
   belongs_to :user
   belongs_to :submission
+
+  validates :file_type, presence: true, inclusion: { in: %w(zip rar tar 7z jpeg gif tiff png archive file), message: "%{value} is not a usable file." }
+  validates :digest, presence: true, length: { is: 64 }, format: { with: /\A[0-9A-Fa-f]+\z/, message: "Not a SHA-256 digest."}
+  validates :apikey, presence: true, length: { is: 64 }, format: { with: /\A[0-9A-Fa-f]+\z/, message: "Not a valid API Key."}
+  validates :size, presence: true, numericality: { only_integer: true }
 end
