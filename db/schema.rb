d2495b79ebd161823c6b5798b0cdee6282583291
John Colagioia <john@colagioia.net>
2015-11-11 06:53:54 -0500

    Clean up style for schema script

--- db/schema.rb
+++ db/schema.rb
@@ -1,15 +1,18 @@
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
+# incrementally modify your database, and then regenerate this schema
+# definition.
 #
 # Note that this schema.rb definition is the authoritative source for your
 # database schema. If you need to create the application database on another
 # system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# from scratch. The latter is a flawed and unsustainable approach (the more
+# migrations you'll amass, the slower it'll run and the greater likelihood
+# for issues).
 #
-# It's strongly recommended that you check this file into your version control system.
+# It's strongly recommended that you check this file into your version
+# control system.
 
 ActiveRecord::Schema.define(version: 20_150_606_132_754) do
 
@@ -82,7 +85,9 @@ ActiveRecord::Schema.define(version: 20_150_606_132_754) do
     t.string 'apikey'
   end
 
-  add_index 'submissions', ['submission_id'], name: 'index_submissions_on_submission_id'
+  add_index 'submissions',
+            ['submission_id'],
+            name: 'index_submissions_on_submission_id'
   add_index 'submissions', ['user_id'], name: 'index_submissions_on_user_id'
 
   create_table 'tracks', force: :cascade do |t|
@@ -112,6 +117,8 @@ ActiveRecord::Schema.define(version: 20_150_606_132_754) do
   end
 
   add_index 'users', ['email'], name: 'index_users_on_email', unique: true
-  add_index 'users', ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
-
+  add_index 'users',
+            ['reset_password_token'],
+            name: 'index_users_on_reset_password_token',
+            unique: true
 end
