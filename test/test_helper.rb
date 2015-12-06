aa001eed34a3bc9908d2a15b39bf4d9204279871
John Colagioia <john@colagioia.net>
2015-12-06 07:37:50 -0500

    Clean up test helper script

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,16 @@
+# Encoding: utf-8
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+# Extension for ActiveSupport?
+class ActiveSupport
+  # Test cases for ActiveSupport
+  class TestCase
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical
+    #   order.
+    fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+    # Add more helper methods to be used by all tests here...
+  end
 end
