b2df49e221ba2d5c872eb69539fe2daf186f2312
John Colagioia <john@colagioia.net>
2015-12-15 06:12:18 -0500

    Clean up submission controller script

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,7 +1,12 @@
+# Encoding: utf-8
+# Submission Controller
 class SubmissionsController < ApplicationController
   before_action :set_submission, only: [:show, :edit, :update, :destroy]
-  skip_before_action :verify_authenticity_token, if: proc { |c| c.request.format == 'application/json' }, only: [:create]
-  protect_from_forgery with: :null_session, if: proc { |c| c.request.format == 'application/json' }
+  skip_before_action :verify_authenticity_token,
+                     if: proc { |c| c.request.format == 'application/json' },
+                     only: [:create]
+  protect_from_forgery with: :null_session,
+                       if: proc { |c| c.request.format == 'application/json' }
 
   # GET /submissions
   # GET /submissions.json
@@ -28,8 +33,9 @@ class SubmissionsController < ApplicationController
   def create
     respond_to do |format|
       format.html { @submission = Submission.new(submission_params) }
-      format.json {
-        if params.key?(:apikey) && User.where(apikey: params[:apikey]).length > 0
+      format.json do
+        if params.key?(:apikey) &&
+           User.where(apikey: params[:apikey]).length > 0
           @submission = Submission.new
           @submission.name = params[:name]
           @submission.file_type = params[:file_type]
@@ -46,16 +52,21 @@ class SubmissionsController < ApplicationController
           render json: ['Invalid API Key'], status: :unprocessable_entity
           return
         end
-      }
+      end
     end
 
     respond_to do |format|
       if @submission.save
-        format.html { redirect_to @submission, notice: 'Submission was successfully created.' }
+        format.html do redirect_to @submission,
+                                   notice: 'Submission was successfully'\
+                                             'created.'
+        end
         format.json { render :show, status: :created, location: @submission }
       else
         format.html { render :new }
-        format.json { render json: @submission.errors, status: :unprocessable_entity }
+        format.json do render json: @submission.errors,
+                              status: :unprocessable_entity
+        end
       end
     end
   end
@@ -65,11 +76,16 @@ class SubmissionsController < ApplicationController
   def update
     respond_to do |format|
       if @submission.update(submission_params)
-        format.html { redirect_to @submission, notice: 'Submission was successfully updated.' }
+        format.html do redirect_to @submission,
+                                   notice: 'Submission was successfully'\
+                                           'updated.'
+        end
         format.json { render :show, status: :ok, location: @submission }
       else
         format.html { render :edit }
-        format.json { render json: @submission.errors, status: :unprocessable_entity }
+        format.json do render json: @submission.errors,
+                              status: :unprocessable_entity
+        end
       end
     end
   end
@@ -79,19 +95,27 @@ class SubmissionsController < ApplicationController
   def destroy
     @submission.destroy
     respond_to do |format|
-      format.html { redirect_to submissions_url, notice: 'Submission was successfully destroyed.' }
+      format.html do redirect_to submissions_url,
+                                 notice: 'Submission was successfully' \
+                                           'destroyed.'
+      end
       format.json { head :no_content }
     end
   end
 
   private
+
   # Use callbacks to share common setup or constraints between actions.
   def set_submission
     @submission = Submission.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
+  # Never trust parameters from the scary internet, only allow the white
+  # list through.
   def submission_params
-    params.require(:submission).permit(:name, :file_type, :hash, :imageError, :size, :height, :width, :page, :modified, :folder, :user_id, :submission_id)
+    params.require(:submission).permit(:name, :file_type, :hash,
+                                       :imageError, :size, :height,
+                                       :width, :page, :modified,
+                                       :folder, :user_id, :submission_id)
   end
 end
