421670d373c7a35bb2c6b7e0f8f2a967c9b93d31
John Colagioia <john@colagioia.net>
2016-01-03 08:22:55 -0500

    Add class comment to callback controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,3 +1,4 @@
+# Omniauth Callback Controller
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
