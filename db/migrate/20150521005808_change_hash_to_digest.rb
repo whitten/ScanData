8d837f4498f4f11fb7f76adb2b6f8fcd9df97b1a
John Colagioia <john@colagioia.net>
2016-01-18 07:01:25 -0500

    Clean up digest migration script

--- db/migrate/20150521005808_change_hash_to_digest.rb
+++ db/migrate/20150521005808_change_hash_to_digest.rb
@@ -4,6 +4,7 @@ class ChangeHashToDigest < ActiveRecord::Migration
   def self.up
     rename_column :submissions, :hash, :digest
   end
+
   def self.down
     rename_column :submissions, :digest, :hash
   end
