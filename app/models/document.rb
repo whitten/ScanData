2e471d10ce83ce2334e610889001b05519b1996a
John Colagioia <john@colagioia.net>
2016-02-02 07:00:11 -0500

    Clean up document model

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,4 @@
+# Document model
 class Document < ActiveRecord::Base
   validates :file_type, presence: true, inclusion: { in: %w(zip rar tar 7z jpeg gif tiff png archive file), message: '%{value} is not a usable file.' }
   validates :digest, presence: true, length: { is: 64 }, format: { with: /\A[0-9A-Fa-f]+\z/, message: 'Not a SHA-256 digest.' }
